06e92953af5a2557498485cccd1efeba965535f3
Dennis <dennisrdown@gmail.com>
2021-12-05 21:01:24 -0500

    initial function to generate markdown completed

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,40 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return 
+  `
+  # ${data.title}
+  https://github.com/${data.github}/${data.title}
+
+  # Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributions](#contribution)
+  - [Test](#tests)
+  - [Questions](#questions)
+
+
+  ## Installation
+  ##### To utilize this application, you will need to install the following dependicies: ${data.installation}
+
+  ## Usage 
+  To use this application: ${data.usage}
+
+  ## License
+  This application has a license of ${data.license}
+
+  ## Contributing
+  Contributions: ${data.contibuting}
+
+  ## Tests
+  Instructions for testing is as follows: ${data.tests}
+
+  ##Questions
+  For questions concerning the application, open an issue or contact ${data.username} at ${data.email}
 `;
 }
 
